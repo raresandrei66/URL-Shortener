23fb7de12594d4aab057df267d7dd91b46f18fb6
raresandrei66 <raresandrei6.ra@gmail.com>
2023-04-11 02:31:29 +0300

    first Commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+
+
+def shorten_link(full_link, link_name):
+    API_KEY = '8e00a768bd2e20709b5971a4cdbc1a5a'
+    BASE_URL = 'https://cutt.ly/api/api.php'
+
+    payload = {'key': API_KEY, 'short': full_link, 'name': link_name}
+    request = requests.get(BASE_URL, params=payload)
+    data = request.json()
+
+    print('')
+
+    try:
+        title = data['url']['title']
+        short_link = data['url']['shortLink']
+
+        print('Title:', title)
+        print('Link:', short_link)
+    except:
+        status = data['url']['status']
+        print('Error Status:', status)
+
+
+link = input('Enter a link: ')
+name = input('Give your link a name: ')
+shorten_link(link, name)
